977ca47165ba6a3ea1ee8261cde1968994d109da
KipCrossing <kip.crossing@gmail.com>
2020-04-02 21:48:33 +1100

    Users service working

--- test.py
+++ test.py
@@ -3,14 +3,11 @@ from bill.get import get
 import json
 
 print(create({"body": json.dumps(
-    {
-        "s8769": "Kip",
-        "r2342": "Bill2"
-    }
+    {"First Name": "Bob", "Last Name": "Katter", "Age": 58, "Sex": "M", "Location": "Sydney"}
 )}, None))
 
 
 print(get({"pathParameters":
            {
-               "id": "c87ace5a-70d7-11ea-8026-681729a3bf9c"
+               "id": ""
            }}, None))
