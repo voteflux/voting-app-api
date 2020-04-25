282d320bc589fcd63faa89802be865c5913527da
KipCrossing <kip.crossing@gmail.com>
2020-04-26 08:53:19 +1000

    Simplify Bill/Issue list output

--- bill/list.py
+++ bill/list.py
@@ -11,24 +11,12 @@ collection = db[mongosettings[BILLSCOLLECTION]]
 
 def list(event, context):
     # create response body object
-    response_body = {}
-
-    # create array for reponse bills
-    response_body['response_bills'] = []
-
-    # return path parameters with filter key
-    response_body['filter'] = event['multiValueQueryStringParameters']
-
-    # build query with any path parameters
-    query = {}
-    if event['multiValueQueryStringParameters'] is not None:
-        for parameter in event['multiValueQueryStringParameters']:
-            query[parameter] = event['multiValueQueryStringParameters'][parameter][0]
+    response_body = []
 
     # create list of bills
-    cursor = collection.find(query)
+    cursor = collection.find()
     for document in cursor:
-        response_body['response_bills'].append(document)
+        response_body.append(document)
 
     # create response
     response = {
