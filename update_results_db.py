4277ea99b14779f22acd63d7e4a73baa5107895d
KipCrossing <kip.crossing@gmail.com>
2020-04-26 21:07:54 +1000

    Add yes/no to Bills/Issues collestions

--- update_results_db.py
+++ update_results_db.py
@@ -10,6 +10,8 @@ db = client[mongosettings[MONGODB]]
 results_collection = db[mongosettings[RESULTSCOLLECTION]]
 ballotspecs_collection = db[mongosettings[BALLOTSPECSCOLLECTION]]
 votes_collection = db[mongosettings[VOTESCOLLECTION]]
+bills_collection = db[mongosettings[BILLSCOLLECTION]]
+issues_collection = db[mongosettings[ISSUESCOLLECTION]]
 CONSTITUENCY = "Australia"
 
 # dummy function, waiting for votes to be counted on the blockchain.
@@ -32,3 +34,14 @@ for ballot in ballotspecs_collection.find():
     (result_doc["yes"], result_doc["no"]) = get_votes(ballot["_id"])
     print(result_doc["_id"], result_doc["yes"], result_doc["no"])
     results_collection.replace_one({'_id': result_doc["_id"]}, {'data': result_doc}, True)
+    if result_doc["constituency"] == CONSTITUENCY:
+        if result_doc["_id"][0] == "i":
+            issues_collection.update_one({'_id': result_doc["_id"]},
+                                         {"$set": {"data.yes": result_doc["yes"]}})
+            issues_collection.update_one({'_id': result_doc["_id"]},
+                                         {"$set": {"data.no": result_doc["no"]}})
+        elif result_doc["_id"][0] == "s" or result_doc["_id"][0] == "r":
+            bills_collection.update_one({'_id': result_doc["_id"]},
+                                        {"$set": {"data.yes": result_doc["yes"]}})
+            bills_collection.update_one({'_id': result_doc["_id"]},
+                                        {"$set": {"data.no": result_doc["no"]}})
