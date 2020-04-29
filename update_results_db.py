d6466017b84f1969c45da6def7b1739434d6a3a2
KipCrossing <kip.crossing@gmail.com>
2020-04-29 15:35:58 +1000

    lambdas for updating bills and results

--- update_results_db.py
+++ update_results_db.py
@@ -18,7 +18,6 @@ CONSTITUENCY = "Australia"
 
 
 def get_votes(id):
-
     yes = len(list(votes_collection.find(
         {"data.ballot_id": id, "data.vote": "yes"}, {"_id": 0, "data.vote": 1}))) + 1
     no = len(list(votes_collection.find(
@@ -26,22 +25,23 @@ def get_votes(id):
     return(yes, no)
 
 
-# print(all_results)
-for ballot in ballotspecs_collection.find():
-    result_doc = {}
-    result_doc["_id"] = ballot["_id"]
-    result_doc["constituency"] = CONSTITUENCY
-    (result_doc["yes"], result_doc["no"]) = get_votes(ballot["_id"])
-    print(result_doc["_id"], result_doc["yes"], result_doc["no"])
-    results_collection.replace_one({'_id': result_doc["_id"]}, {'data': result_doc}, True)
-    if result_doc["constituency"] == CONSTITUENCY:
-        if result_doc["_id"][0] == "i":
-            issues_collection.update_one({'_id': result_doc["_id"]},
-                                         {"$set": {"data.yes": result_doc["yes"]}})
-            issues_collection.update_one({'_id': result_doc["_id"]},
-                                         {"$set": {"data.no": result_doc["no"]}})
-        elif result_doc["_id"][0] == "s" or result_doc["_id"][0] == "r":
-            bills_collection.update_one({'_id': result_doc["_id"]},
-                                        {"$set": {"data.yes": result_doc["yes"]}})
-            bills_collection.update_one({'_id': result_doc["_id"]},
-                                        {"$set": {"data.no": result_doc["no"]}})
+def run(event, context):
+    # print(all_results)
+    for ballot in ballotspecs_collection.find():
+        result_doc = {}
+        result_doc["_id"] = ballot["_id"]
+        result_doc["constituency"] = CONSTITUENCY
+        (result_doc["yes"], result_doc["no"]) = get_votes(ballot["_id"])
+        print(result_doc["_id"], result_doc["yes"], result_doc["no"])
+        results_collection.replace_one({'_id': result_doc["_id"]}, {'data': result_doc}, True)
+        if result_doc["constituency"] == CONSTITUENCY:
+            if result_doc["_id"][0] == "i":
+                issues_collection.update_one({'_id': result_doc["_id"]},
+                                             {"$set": {"data.yes": result_doc["yes"]}})
+                issues_collection.update_one({'_id': result_doc["_id"]},
+                                             {"$set": {"data.no": result_doc["no"]}})
+            elif result_doc["_id"][0] == "s" or result_doc["_id"][0] == "r":
+                bills_collection.update_one({'_id': result_doc["_id"]},
+                                            {"$set": {"data.yes": result_doc["yes"]}})
+                bills_collection.update_one({'_id': result_doc["_id"]},
+                                            {"$set": {"data.no": result_doc["no"]}})
