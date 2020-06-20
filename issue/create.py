8a701df56c8d7ae9d77009726215b05cd19d49a9
KipCrossing <kip.crossing@gmail.com>
2020-06-20 21:11:14 +1000

    Handel create issue errors better

--- issue/create.py
+++ issue/create.py
@@ -23,58 +23,65 @@ issues_document = {"_id": "i4",
 
 def create(event, context):
     # get request body
-    payload = json.loads(event['body'])
+    try:
+        payload = json.loads(event['body'])
 
-    TOKEN = payload["token"]
+        TOKEN = payload["token"]
 
-    if ISSUE_TOKEN == TOKEN:
+        if ISSUE_TOKEN == TOKEN:
 
-        issue = payload["data"]
+            issue = payload["data"]
 
-        total_issues = len(list(collection.find()))
+            total_issues = len(list(collection.find()))
 
-        titles = [items["data"]["short_title"] for items in list(collection.find())]
-        print(list(titles))
-        if issue["short_title"] not in titles:
-            if total_issues:
-                print([items["num"] for items in list(collection.find())])
-                issue_number = collection.find().sort("num", -1)[0]["num"] + 1
-                print(issue_number)
+            titles = [items["data"]["short_title"] for items in list(collection.find())]
+            print(list(titles))
+            if issue["short_title"] not in titles:
+                if total_issues:
+                    print([items["num"] for items in list(collection.find())])
+                    issue_number = collection.find().sort("num", -1)[0]["num"] + 1
+                    print(issue_number)
+                else:
+                    issue_number = 1
+                issue_id = "i" + str(issue_number)
+                issue["id"] = issue_id
+                collection.insert_one({'_id': issue["id"],
+                                    "num": issue_number,
+                                    'data': issue})
+
+                update_ballotspecs(issue["id"], issue["short_title"], issue["question"],
+                                issue["description"], issue["start_date"], issue["chamber"], issue["sponsor"])
+                response = {
+                    "statusCode": 200,
+                    "body": json.dumps(issue)
+                }
             else:
-                issue_number = 1
-            issue_id = "i" + str(issue_number)
-            issue["id"] = issue_id
-            collection.insert_one({'_id': issue["id"],
-                                   "num": issue_number,
-                                   'data': issue})
+                # collection.update_one({'data.short_title': issue["short_title"]},
+                #                       {"$set": {'data': issue}}, True)
 
-            update_ballotspecs(issue["id"], issue["short_title"], issue["question"],
-                               issue["description"], issue["start_date"], issue["chamber"], issue["sponsor"])
+                response = {
+                    "statusCode": 409,
+                    "body": json.dumps("short_title already exists")
+                }
+        elif TOKEN == 'TEST':
+            
+            issue = payload["data"]
+
             response = {
                 "statusCode": 200,
                 "body": json.dumps(issue)
             }
+
         else:
-            # collection.update_one({'data.short_title': issue["short_title"]},
-            #                       {"$set": {'data': issue}}, True)
-
             response = {
-                "statusCode": 409,
-                "body": json.dumps("short_title already exists")
+                "statusCode": 401,
+                "body": json.dumps("Invalid token")
             }
-    elif TOKEN == 'TEST':
-        
-        issue = payload["data"]
 
+    except Exception as e:
         response = {
-            "statusCode": 200,
-            "body": json.dumps(issue)
-        }
-
-    else:
-        response = {
-            "statusCode": 401,
-            "body": json.dumps("Invalid token")
+            "statusCode": 500,
+            "body": str(e)
         }
     # return response
     return response
