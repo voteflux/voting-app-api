95ae6224b1fa29ccd5e9671f50cfa2ee495154c8
KipCrossing <kip.crossing@gmail.com>
2020-06-20 20:19:32 +1000

    add spsc endpoint for baz

---
+++ spec/list.py
@@ -0,0 +1,28 @@
+import json
+import os
+import pymongo
+from mode import *
+
+# Connection String
+client = pymongo.MongoClient(mongosettings[URL])
+db = client[mongosettings[MONGODB]]
+collection = db[mongosettings[BALLOTSPECSCOLLECTION]]
+
+
+def list(event, context):
+    # create response body object
+    response_body = []
+
+    # create list of ballots
+    cursor = collection.find()
+    for document in cursor:
+        response_body.append(document)
+
+    # create response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(response_body)
+    }
+
+    # return response
+    return response
