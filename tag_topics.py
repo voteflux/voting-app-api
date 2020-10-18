6ca6ec5fc52e5d2bb0d29415cfedb97a93e5d1fc
kipling <kip.crossing@gmail.com>
2020-10-18 18:11:29 +1100

    typing and docs

--- tag_topics.py
+++ tag_topics.py
@@ -1,5 +1,6 @@
 import requests
 from operator import itemgetter
+from typing import Dict, List
 
 # Topics 
 citizens = ["Education","Skills","Employment","Health", "Veterans", "youth","sports","Social","Indigenous","Disability"]
@@ -12,32 +13,40 @@ communications = ["Communications"]
 topics = {
     "citizens":citizens,
     "nature":nature,
-    "national_development":national_development,
+    "national development":national_development,
     "borders":borders,
     "economy":economy,
     "communications":communications,
 }
 
-# related terms 
-rt = open("related_terms.csv","r")
-topic_lines = rt.readlines()
-headings = topic_lines.pop(0)
-headings_list = headings.replace("\n","").lower().split(",")
+def tag_bill(bill: Dict) -> List[str]:
+    """To make tags for a bill based on the bill protfolio, title and text
 
-topic_models = {}
-for i in range(len(headings.replace("\n","").split(","))):
-    terms = []
-    for tl in topic_lines:
-        term = tl.replace("\n","").split(",")[i]
-        if term != "":
-            terms.append(term.lower())
-    topic_models[headings_list[i]] = terms
+    Args:
+        bill (Dict): A dict with the bill info. Need "short_title","summary". "portfolio" is preferred too
 
+    Returns:
+        List[str]: The tags 
+    """
+    # * get the related terms 
+    rt = open("related_terms.csv","r")
+    topic_lines = rt.readlines()
+    headings = topic_lines.pop(0)
+    headings_list = headings.replace("\n","").lower().split(",")
 
-def tag_bill(bill):
+    topic_models = {}
+    for i in range(len(headings.replace("\n","").split(","))):
+        terms = []
+        for tl in topic_lines:
+            term = tl.replace("\n","").split(",")[i]
+            if term != "":
+                terms.append(term.lower())
+        topic_models[headings_list[i]] = terms
+
     t = []
     if "portfolio" in bill.keys():
         if bill["portfolio"] != "":
+            # * use portfolio to make the tags
             portfoilo = bill["portfolio"]
             for topic in topics.keys():
                 for domain in topics[topic]:
@@ -47,6 +56,7 @@ def tag_bill(bill):
     if t != []:
         return(t)
     else:
+        # * use 
         p = []
         for h in headings_list:
             for rt in topic_models[h]:
@@ -54,4 +64,3 @@ def tag_bill(bill):
                     if h not in p:
                         p.append(h)
         return(p)
-                
